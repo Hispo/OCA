db7bd0d4ef2233fa9012b06ab8420776cb5c0e01
Edson F <edson_nicholas@hotmail.com>
2017-12-05 18:12:03 -0200

    Add files via upload

---
+++ OCA/TryStatement.java
@@ -0,0 +1,34 @@
+// Reconstruindo a queda da garota
+// Page 306
+
+public class TryStatement {
+
+	void girl(){
+		try{
+			fall();
+		} catch (RuntimeException e) {
+			getUp();
+		} 
+		// Reversing the catchs will cause a compilation error.
+		//error: exception RuntimeException has already been caught.
+		catch (Exception ex) { 
+			System.out.println("Exception");
+		} finally {
+			System.out.println("The girl is already up.");
+		}
+	} // fim girl()
+
+	void fall(){
+		System.out.println("Shit, the girl fell.");
+		throw new RuntimeException();
+	}
+
+	void getUp(){
+		System.out.println("Come on girl, get up.");
+	}
+
+	public static void main(String[] args) {
+		TryStatement _try = new TryStatement();
+		_try.girl();
+	}
+}
